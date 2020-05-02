ba26ca62d7ae14182b0ac9add4f822894e870812
Thiago Ramalho <thiago.nramalho@gmail.com>
2020-05-01 22:14:56 -0300

    updated sponsor

--- packages/qrt-molecules/sponsor/Sponsor.js
+++ packages/qrt-molecules/sponsor/Sponsor.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import { string } from 'prop-types';
+import React from 'react';
 
 import Image from '@qrt/atoms/image';
 import Typography from '@qrt/atoms/typography';
